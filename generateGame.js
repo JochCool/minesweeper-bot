45bdb2f1c61ef7eadd5c01e364554fa71cc954b3
Jochem <29899660+JochCool@users.noreply.github.com>
2022-02-15 21:24:22 +0100

    Fix inconsistency in game width/height defaults

--- generateGame.js
+++ generateGame.js
@@ -12,7 +12,7 @@ module.exports = function generateGame(gameWidth, gameHeight, numMines, source, 
 	
 	// Check game size
 	if (isNaN(gameWidth)) {
-		gameWidth = 8;
+		gameWidth = isNaN(gameHeight) ? 8 : gameHeight;
 	}
 	else if (gameWidth <= 0 || gameHeight <= 0) {
 		return `Uh, I'm not smart enough to generate a maze sized ${gameWidth} by ${gameHeight}. I can only use positive numbers. Sorry :cry:`;
